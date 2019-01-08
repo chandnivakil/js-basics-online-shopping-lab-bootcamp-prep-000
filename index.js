56108ffc27906f53fa339ab204d83faa07d10aaa
fantastic-page-5704 <chandvakil@gmail.com>
2019-01-08 20:23:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,10 +10,21 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ item = Object.assign({}, { itemName: item }, { itemPrice: Math.floor(Math.random() * 101) }) 
+ cart.push(item)
+ return `${item.itemName} has been added to your cart.`
 }
+ 
 
 function viewCart() {
+  var inCart = ""
+  for(var i = 0; i < cart.length; i++) {
+    if (cart.length === 1) { 
+    return `In your cart you have ${item.itemName} at ${item.itemPrice}.`
+    }
+  if (cart.length > 1) {
+    inCart = inCart + `${item.itemName}` + "at" `${item.itemPrice}` + "," + "and"
+  }
   // write your code here
 }
 
